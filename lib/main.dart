ad59bedce5c504e506942a0302ace7fe05aa016e
BispoCarlosDev <carlos.oliveirabispo@hotmail.com>
2024-04-12 20:11:47 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Formulario());
+}
+
+class Formulario extends StatefulWidget {
+  _FormularioState createState() => _FormularioState();
+}
+
+class _FormularioState extends State<Formulario> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Form(
+          key: _formKey,
+          child: TextFormField(
+            decoration: InputDecoration(
+              hintText: "Informe o seu nome",
+            ),
+            validator: (value) {
+              if (value!.isEmpty){
+                return "Preencha corretamente!";
+              }
+            }
+          )),
+      ),
+    );
+  }
+}
